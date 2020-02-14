3e0fc285300052a049eaf7a7ed929b901c7f61eb
gdi1234 <lee123genre@gmail.com>
2020-02-14 12:35:34 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,54 @@
+const api={
+    key:"7077b29491b5835187bd0205137b1c04",
+    baseurl:"http://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox=document.querySelector('.search-box');
+searchbox.addEventListener('keypress',setQuery);
+
+function setQuery(evt){
+    if(evt.keyCode==13){
+        getResults(searchbox.value);
+    }
+}
+
+function getResults(query){
+    fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather=>{
+        return weather.json();
+    }).then(displayResults);
+    .catch(function(error){
+        console.log(error);
+        alert(`"${city}" Weather Data Not Found.`);
+    });
+}
+
+function displayResults(weather){
+    console.log(weather);
+    let city=document.querySelector('.location .city');
+    city.innerText=`${weather.name},${weather.sys.country}`;
+
+    let now=new Date();
+    let date=document.querySelector('.location .date');
+    date.innerText=dataBuilder(now);
+
+    let temp=document.querySelector('.current .temp');
+    temp.innerHTML=`${Math.round(weather.main.temp)}<span>°C</span>`;
+
+    let weather_el=document.querySelector('.current .weather');
+    weather_el.innerText=weather.weather[0].main;
+
+    let high_low=document.querySelector('.current .high-low');
+    high_low.innerText=`${Math.round(weather.main.temp_min)}°C/${Math.round(weather.main.temp_max)}°C`;
+}
+function dataBuilder(d){
+    let months=["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+    let day=days[d.getDay()];
+    let date=d.getDate();
+    let month=months[d.getMonth()];
+    let year=d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
